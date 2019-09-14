a4bd09e65ed3027e3c4adb2ec31c934ee52e7ce9
d.timofeev <Dima27.07.90@mail.ru>
2019-09-14 09:36:16 +0300

    Добавил роут для получения дифа

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const {resolve: pathResolve} = require('path');
 const routes = require('./src/routes.json');
 const getCommits = require('./src/git/getCommit.js');
+const getDiff = require('./src/git/getDiff.js');
 const getReposList = require('./src/getReposList.js');
 const env = require('./src/env.js');
 if (!argv.path || argv.path.length === 0) {
@@ -34,6 +35,15 @@ const reposDir = pathResolve(__dirname, argv.path);
         /** @param {{params:{repositoryId:string, commitHash:string}}} req */
         getCommits(pathResolve(reposDir, req.params.repositoryId), req.params.commitHash).then(commits => {
             res.json(commits);
+        }).catch(reason => {
+            res.json({error: reason});
+        });
+    });
+    server.get(routes.commitDiff, (req, res) => {
+        //todo тут бы проверку как минимум на существование папки сделать
+        /** @param {{params:{repositoryId:string, commitHash:string}}} req */
+        getDiff(pathResolve(reposDir, req.params.repositoryId), req.params.commitHash).then(dataDiff => {
+            res.json(dataDiff);
         }).catch(reason => {
             res.json({error: reason});
         });
---
+++ src/git/getDiff.js
@@ -0,0 +1,50 @@
+const {execFile, execFileSync} = require('child_process');
+/**
+ *
+ * @param {string} reposDir
+ * @param {string} commitHash
+ * @returns {Promise}
+ */
+module.exports = (reposDir, commitHash) => {
+    let options = [
+        commitHash,
+        '--raw'
+    ];
+    options.unshift('diff');
+    return (new Promise((resolve) => {
+        execFile('git', options, {cwd: reposDir}, (err, data) => {
+            let diffFiles = data.split("\n")
+                .reduce((previousValue, currentValue) => {
+                    currentValue = currentValue.trim();
+                    if (currentValue.length === 0) {
+                        return previousValue;
+                    }
+                    let valueSplit = currentValue.split("\t");
+                    let mode = valueSplit[0].split(' ').pop()[0];
+                    let filePath;
+                    let commitFile = valueSplit[1];
+                    if (mode === 'R') {
+                        filePath = {
+                            from: commitFile,
+                            to: valueSplit[2],
+                        };
+                    } else {
+                        filePath = commitFile;
+                    }
+                    let diff = getDiffByPath(reposDir,commitHash,filePath);
+                    previousValue[commitFile] = {filePath, mode, diff};
+                    return previousValue;
+                }, {});
+            resolve(diffFiles);
+        });
+    }));
+};
+const getDiffByPath = (reposDir, commitHash, path) => {
+    let options = [
+        commitHash,
+        '--unified=0',
+        path
+    ];
+    options.unshift('diff');
+    return execFileSync('git', options, {cwd: reposDir}).toString();
+};
